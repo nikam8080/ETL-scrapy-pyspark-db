1ec878bf7a0dda628374e74625818fd3e9a5877d
nikam8080 <nikhilnikam@Nikhils-MBP-2.lan>
2024-02-15 16:46:26 +0530

    Added some changes in flipkart spider

--- scrapy/Audio_Data/Audio_Data/spiders/flipkart_Audio_spider.py
+++ scrapy/Audio_Data/Audio_Data/spiders/flipkart_Audio_spider.py
@@ -7,14 +7,19 @@ from ..items import AudioDataItem
 class FlipkartScrawling(scrapy.Spider):
     name = 'flipkart'
 
+    page_number = 2
+
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.9999.99 Safari/537.36',
     }
 
-    start_urls = [
-        'https://www.flipkart.com/headset/pr?sid=fcn&otracker=categorytree'
-    ]
+    # start_urls = [
+    #     'https://www.flipkart.com/headset/pr?sid=fcn&otracker=categorytree&page=1'
+    # ]
 
+    start_urls = [
+        'https://www.flipkart.com/search?q=audio&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=1'
+    ]
 
     def parse(self, response):
         items = AudioDataItem()
@@ -24,6 +29,7 @@ class FlipkartScrawling(scrapy.Spider):
         offer_price = response.css("div[class=_30jeq3]::text").extract()
         # discount = response.css("span[class='_1lRcqv']::text").extract()
         reviews_count = response.css("[class=_2_R_DZ]::text").extract()
+        audio_type = response.css("[class=_3Djpdu]::text").extract()
 
         formatted_actual_price = []
 
@@ -31,13 +37,14 @@ class FlipkartScrawling(scrapy.Spider):
             formatted_actual_price.append(actual_price[i] + actual_price[i + 1])
 
 
-        for item in zip(audio_names,formatted_actual_price, offer_price, reviews_count):
+        for item in zip(audio_names,formatted_actual_price, offer_price, reviews_count, audio_type):
             scrapped_info = {
                 'audio_name': item[0],
                 'formatted_actual_price': item[1],
                 'offer_price': item[2],
                 # 'rating': item[3],
-                'reviews_count': item[3]         
+                'reviews_count': item[3],
+                'audio_type': item[4]        
             }
             
             yield scrapped_info
@@ -47,5 +54,16 @@ class FlipkartScrawling(scrapy.Spider):
         items['offer_price'] = offer_price
         # items['discount'] = discount
         items['reviews_count'] = reviews_count
+        items['audio_type'] = audio_type
 
         yield items
+
+        # next_page = "https://www.flipkart.com/headset/pr?sid=fcn&otracker=categorytree&page=1"+str(FlipkartScrawling.page_number)
+        # if FlipkartScrawling.page_number <= 3:
+        #     FlipkartScrawling.page_number += 1
+        #     yield response.follow(next_page, callback = self.parse)
+
+        next_page = "https://www.flipkart.com/search?q=audio&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&page=1"+str(FlipkartScrawling.page_number)
+        if FlipkartScrawling.page_number <= 50:
+            FlipkartScrawling.page_number += 1
+            yield response.follow(next_page, callback = self.parse)
